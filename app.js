2a7f49d6532feace280a8d7cd2f3203edb0f683b
Vanama Akshay Kumar <vanama.akshay06@gmail.com>
2025-06-23 17:06:36 +0530

    Add root index

--- app.js
+++ app.js
@@ -80,9 +80,9 @@ app.use((req,res,next)=>{
 })
 
 // root index
-// app.get("/",(req,res)=>{
-//     res.send("Hi, I am root");
-// })
+app.get("/",(req,res)=>{
+    res.redirect("/listings");
+})
 
 // app.get("/demoUser",async (req,res)=>{
 //     let fakeUser=new User({
